2bc78b2a9b3b231e36e6a646218aa35fa0e86f12
idiallo0123 <idiallo0123@bths.edu>
2018-12-05 08:41:31 -0500

    yerr

--- src/com/company/FaceDrawing.java
+++ src/com/company/FaceDrawing.java
@@ -11,16 +11,29 @@ public class FaceDrawing
 
     void edit(String replace, int row, int column)
     {
-
+        fFeatures[row][column] = replace;
     }
 
     void fill (String str)
     {
-
+        str = " ";
+        for(int i = 0; i < fFeatures.length; i++)
+        {
+            for(int n = 0; i < fFeatures[i].length; n++)
+            {
+                fFeatures[i][n] = str;
+            }
+        }
     }
 
     String toString()
     {
-        
+        for(int i = 0; i < fFeatures.length; i++)
+        {
+            for(int n = 0; i < fFeatures[i].length; n++)
+            {
+                System.out.print(fFeatures[i][n]);
+            }
+        }
     }
 }
--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -2,7 +2,9 @@ package com.company;
 
 public class Runner {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+	String[] [] face = new String[5][8];
+    FaceDrawing face1 =
     }
 }
